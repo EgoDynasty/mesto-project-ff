bc1d753ca4d46f659eabbfc54ec0c572ec6829f7
Egorich <tv.titov2015@yandex.ru>
2024-11-10 07:41:38 +0300

    first try

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+
+function deleteCard(event) {
+  const cardElement = event.target.closest(".card");
+  if (cardElement) {
+    cardElement.remove();
+  }
+}
+
+function createCard(data) {
+  const template = document.getElementById("card-template");
+  const cardElement = template.content.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData);
+  placesList.appendChild(cardElement);
+});
