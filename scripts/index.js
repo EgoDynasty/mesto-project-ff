f5376143555a5421f192a8ca1de93a498a8b0a11
Egorich <tv.titov2015@yandex.ru>
2024-11-10 19:23:53 +0300

    onDelete fixes

--- scripts/index.js
+++ scripts/index.js
@@ -17,9 +17,9 @@ function deleteCard(event) {
   }
 }
 
-function createCard(data) {
-  const template = document.getElementById("card-template");
-  const cardElement = template.content.cloneNode(true);
+function createCard(data, onDelete) {
+  const template = document.getElementById('card-template').content;
+  const cardElement = template.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
   const deleteButton = cardElement.querySelector(".card__delete-button");
@@ -28,11 +28,11 @@ function createCard(data) {
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
 
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", onDelete);
   return cardElement;
 }
 
 initialCards.forEach((cardData) => {
-  const cardElement = createCard(cardData);
+  const cardElement = createCard(cardData, deleteCard);
   placesList.appendChild(cardElement);
 });
