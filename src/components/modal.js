90ad6ce5a3e5fe87a061fdb9c00fb49daab022fa
Egorich <tv.titov2015@yandex.ru>
2025-01-15 03:13:01 +0300

    removeimport

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,3 @@
-import { validationConfig, clearValidation } from "./validation";
-
 function handleEscape(evt) {
   if (evt.key === 'Escape') {
     const openedPopup = document.querySelector('.popup_is-opened');
